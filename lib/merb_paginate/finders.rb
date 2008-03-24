a9c76cd67f73160bfeea3b73123a19f6d01a309c
Nathan Herald <myobie@mac.com>
2008-03-24 18:26:32 -0400

    A stupid type. requre

--- lib/merb_paginate/finders.rb
+++ lib/merb_paginate/finders.rb
@@ -4,10 +4,10 @@ require 'merb_paginate/core_ext'
 # Load in the ORM Helpers as needed
 
 if const_defined? "DataMapper"
-  requre 'merb_paginate/finders/datamapper'; DataMapper::Base.class_eval { include MerbPaginate::Finders::Datamapper }
+  require 'merb_paginate/finders/datamapper'; DataMapper::Base.class_eval { include MerbPaginate::Finders::Datamapper }
 
 if const_defined? "Sequel"
-  requre 'merb_paginate/finders/sequel'; SequelModel::Base.class_eval { include MerbPaginate::Finders::Sequel }
+  require 'merb_paginate/finders/sequel'; SequelModel::Base.class_eval { include MerbPaginate::Finders::Sequel }
 
 if const_defined? "ActiveRecord"
-  requre 'merb_paginate/finders/activerecord'; ActiveRecord::Base.class_eval { include MerbPaginate::Finders::Activerecord }
+  require 'merb_paginate/finders/activerecord'; ActiveRecord::Base.class_eval { include MerbPaginate::Finders::Activerecord }
