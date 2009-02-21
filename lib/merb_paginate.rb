fee5e2a82d7bc87dcb1a883e317bffffe636b853
Rue the Ghetto <ru_ghetto@rubyghetto.com>
2009-02-20 16:31:53 -0800

    fixed to look for ORM after_app_loads, so the finders for AR actually get loaded when listed in config/dependency.rb

--- lib/merb_paginate.rb
+++ lib/merb_paginate.rb
@@ -1,3 +1,23 @@
 require 'will_paginate/collection'
 require 'merb_paginate/finders'
 require 'merb_paginate/view_helpers'
+
+if defined?(Merb::Plugins)
+  Merb::BootLoader.after_app_loads do
+    # Load in the ORM Helpers as needed
+    if Object.const_defined? "DataMapper"
+      require 'merb_paginate/finders/datamapper'
+      DataMapper::Resource.class_eval { include MerbPaginate::Finders::Datamapper }
+    end
+
+    if Object.const_defined? "Sequel"
+      require 'merb_paginate/finders/sequel'
+      SequelModel::Base.class_eval { include MerbPaginate::Finders::Sequel }
+    end
+
+    if Object.const_defined? "ActiveRecord"
+      require 'merb_paginate/finders/activerecord'
+      ActiveRecord::Base.class_eval { include MerbPaginate::Finders::Activerecord }
+    end
+  end
+end
